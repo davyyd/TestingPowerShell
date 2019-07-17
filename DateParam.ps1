8f3dde65520af41758fa981a0a08aa7147bcb57a
Summers <daves@uidaho.edu>
2019-07-17 07:45:11 -0700

    First commit

---
+++ DateParam.ps1
@@ -0,0 +1,10 @@
+﻿[cmdletbinding()]
+Param(
+	[Parameter(HelpMessage = "Enter the date the crash occured, in format yyyy.mm.dd hh.mm.ss (24 hour time).")]
+	[ValidateNotNullorEmpty()]
+	[DateTime] $dumpDate=(Get-Date)
+)
+
+$myString = "The date is " + $dumpDate
+
+Write-Host $myString
